f9c746814417d0b4f7816b3f13ebecb93906a38e
AleksTMN <kirilloff_77@mail.ru>
2022-05-28 13:37:33 +0500

    4

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -6,12 +6,13 @@ import java.util.*;
 public class Main {
     private static final List<Integer> NUMS = List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7, 12, 12);
 
-    private static final List<String> PRODUCT = List.of("мясо", "мясо", "молоко", "хлеб", "яйца", "творог", "творог", "масло");
+    private static final List<String> PRODUCT = List.of("мясо", "масло", "мясо", "молоко", "хлеб", "хлеб", "яйца", "творог", "масло");
 
     public static void main(String[] args) {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static void task1() {
@@ -39,5 +40,9 @@ public class Main {
         System.out.println(product);
     }
 
+    public static void task4() {
+        Set<String> product = new HashSet<>(PRODUCT);
+        System.out.println(PRODUCT.size() - product.size());
+    }
 }
 
